089586f303c13e37cb5fef224f72205ec410508d
mapenggang <1983462054@qq.com>
2018-12-05 10:19:17 +0800

    ASP.NET Core练习实例

---
+++ Sample02/CoreDemo/CoreDemo/Controllers/ContentController.cs
@@ -0,0 +1,30 @@
+﻿using CoreDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Controllers
+{
+    
+    public class ContentController: Controller
+    {
+        private readonly Content contents;
+        public ContentController(IOptions<Content> option)
+        {
+            contents = option.Value;
+        }
+        /// <summary>
+        /// 首页显示
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+
+            return View(new ContentViewModel { Contents = new List<Content> { contents } });
+        }
+    }
+    
+}
